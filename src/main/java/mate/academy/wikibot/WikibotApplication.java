aa5dbf3089bf49f3860e00928876d3b480c94c47
YatKat <30606206+YatKat@users.noreply.github.com>
2019-03-07 12:14:33 +0200

    All changes (#1)
    * added startup class
    
    * added index.html
    
    * name added
    
    * added name in index.html
    
    * add my name
    
    * add my name to index.html
    
    * first commit
    
    * add Kate
    
    * first committ
    
    * resolved conflict
    
    * fourth commit
    
    * added my name in index.html
    
    * added empty line at the EOF
    
    * added mvn checkstyle
    
    * NAWB-6 add http client (#21)
    
    * add http client
    
    * Delete application.properties
    
    * add http client
    
    * add circleCI config file
    
    * add changes to circleCI config file
    
    * Delete application.properties

---
+++ src/main/java/mate/academy/wikibot/WikibotApplication.java
@@ -0,0 +1,12 @@
+package mate.academy.wikibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikibotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikibotApplication.class, args);
+    }
+}
